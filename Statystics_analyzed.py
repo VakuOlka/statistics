a974b956617d770eb32b458b7b11914eaa042b21
Gregory <Gregory108@users.noreply.github.com>
2018-11-18 01:33:49 +0100

    Update Statystics_analyzed.py

--- Statystics_analyzed.py
+++ Statystics_analyzed.py
@@ -7,7 +7,10 @@ import pandas as pd
 import glob
 
 #data = np.loadtxt("Des___00DKO_00709_analyzed.txt", dtype=np.object, delimiter=",",skiprows=1, usecols=(2, 3, 4, 6, 7)) #use length, speed, RD, DP and DI columns from initial file.
-data = np.genfromtxt("00709analyzed.txt", delimiter=",",skip_header=1, usecols=(4, 6, 7)) #use length, speed, RD, DP and DI columns from initial file.
+data = np.genfromtxt("00709analyzed.txt", 
+                     delimiter=",", 
+                     skip_header=1, 
+                     usecols=(4, 6, 7)) #use length, speed, RD, DP and DI columns from initial file.
 #print (data)
 #a = np.array([data])
 rd = data[:, [1]]
