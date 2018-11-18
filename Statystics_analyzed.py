25f8502609a57bcfa79c5274d15d17284852d7b2
VakuOlka <45133053+VakuOlka@users.noreply.github.com>
2018-11-18 01:09:43 +0100

    Update Statystics_analyzed.py
    rd = data[:, [1]] выбирает колонку Dynamic Peak

--- Statystics_analyzed.py
+++ Statystics_analyzed.py
@@ -7,11 +7,11 @@ import pandas as pd
 import glob
 
 #data = np.loadtxt("Des___00DKO_00709_analyzed.txt", dtype=np.object, delimiter=",",skiprows=1, usecols=(2, 3, 4, 6, 7)) #use length, speed, RD, DP and DI columns from initial file.
-data = np.genfromtxt("Des___00DKO_00709_analyzed.txt", delimiter=",",skip_header=1, usecols=(4, 6, 7)) #use length, speed, RD, DP and DI columns from initial file.
+data = np.genfromtxt("00709analyzed.txt", delimiter=",",skip_header=1, usecols=(4, 6, 7)) #use length, speed, RD, DP and DI columns from initial file.
 #print (data)
-a = np.array([data])
-#rd = a[:, [1, 2]]
-#print(rd)
-a.shape
-print(a.shape)
+#a = np.array([data])
+rd = data[:, [1]]
+print(rd)
+#data.shape
+#print(data.shape)
 #rd = np.array(data(usecols=4))
